2349a4184a6c3c48ce3f3a2e0dbce5fe6f2c4cb5
Ulrik-Matemu <advancechronicles@gmail.com>
2025-02-25 00:20:36 +0300

    added check availability button

--- src/components/my-components/booking-form.tsx
+++ src/components/my-components/booking-form.tsx
@@ -1,6 +1,7 @@
 import { Label } from "@/components/ui/label";
 import { DatePickerWithRange } from "@/components/my-components/date-picker";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const BookingForm = () => {
     return (
@@ -18,6 +19,9 @@ export const BookingForm = () => {
                     <Label className="font-semibold" htmlFor="rooms">Number of Children</Label>
                     <Input type="number" id="children" className=" p-2 border border-gray-800 rounded dark:border-gray-600 dark:bg-black" />
                 </div>
+            </div>
+            <div className="grid my-5">
+                <Button className="w-100 bg-black dark:bg-white dark:text-black font-bold">Check Availability</Button>
             </div>
         </>
     );
